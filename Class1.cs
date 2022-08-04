bf1f1a7562b59f78a32f348b1887e10c239d3df3
aarynverma <18100BTCSE02667@svvvindore.onmicrosoft.com>
2022-08-04 16:42:22 +0530

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace ToCheckCurrency
+{
+    public class InvalidInputException : Exception
+    {
+        public InvalidInputException(String Message)
+            : base(Message) { }
+    }
+    public static class ToCheckCurrency
+    {
+
+
+        public static string ToCurrency(this string str)
+        {
+            var isNumber = Regex.IsMatch(str, @"^\d+$");
+
+            if (!isNumber)
+                return str;
+             
+            else
+                throw new InvalidInputException("Invalid Input!");
+
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToCheckCurrency
+{
+    internal class Class2
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the value");
+            string input = Console.ReadLine();
+            string Currency = ToCheckCurrency.ToCurrency(input);
+            Console.WriteLine(Currency);
+        }
+    }
+}
